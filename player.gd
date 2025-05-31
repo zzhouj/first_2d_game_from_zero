c98cfd62b9c1aebf130f5a03b94462ff0dade07e
Justin <457299@qq.com>
2025-05-31 21:34:29 +0800

    player animation

--- player.gd
+++ player.gd
@@ -1,7 +1,13 @@
 extends CharacterBody2D
 
+@onready var happy_boo = $HappyBoo
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * 600
 	move_and_slide()
+	
+	if velocity.length() > 0:
+		happy_boo.play_walk_animation()
+	else:
+		happy_boo.play_idle_animation()
