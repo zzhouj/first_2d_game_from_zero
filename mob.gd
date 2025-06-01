4a3ba332f34fb4ea82690cc10f89e41f160aec08
Justin <457299@qq.com>
2025-06-01 20:02:33 +0800

    format gdscripts

--- mob.gd
+++ mob.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-
 @onready var player = get_node("/root/Game/Player")
+
 
 func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
